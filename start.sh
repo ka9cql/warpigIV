2508bcaae2fb87154239ed3f6fe8653f1867697f
Mike <ka9cql@gmail.com>
2019-05-18 04:37:26 +0000

    Moved lots of things out of /etc/rc.local and put them in start.sh

---
+++ start.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+##################
+# start.sh - Script that gets called by /etc/rc.local to startup all the
+#            direwolf/APRS/packet BBS/etc. stuff.
+#
+# HISTORICAL INFORMATION -
+#
+#  2019-05-17  msipin  Created
+#################
+
+# START DIREWOLF -
+cd /home/direwolf
+(/usr/bin/nohup /home/direwolf -t 0 -qh -qd 2>&1 >> /dev/null)&
+
+# Direwolf opens ports 8000 and 8001
+
+# TO-DO: loop until BOTH tcp port 8000 and tcp port 8001 are in the LISTEN state
+
+/bin/sleep 10
+
+# kissutil REQUIRES Direwolf
+# START KISSUTIL -
+cd /home/direwolf
+(/usr/bin/nohup /home/direwolf/kissutil -f /home/direwolf/outbox/ 2>&1 >> /dev/null)&
+
+/bin/sleep 10
+
+# Then start sendAPRS -
+cd /home/pifm
+(/usr/bin/nohup /home/pifm/sendAPRS)&
+
+/bin/sleep 5
+
+# /home/kiss/examples/test.py REQUIRES Direwolf *and* Kissutil
+# Then start our APRS message-parser/responder
+cd /home/pifm
+(/usr/bin/nohup /usr/bin/python3 /usr/local/bin/msgparser.py 2>&1 >> /usr/local/bin/APRSdata.log)&
+
+/bin/sleep 5
+
+# linbpq REQUIRES Direwolf
+# Although Linbpq does not require Kissutil, I would like to start Linbpq after Kissutil, not before
+# Then start the LinBPQ Packet BBS system -
+cd /home/linbpq
+(/usr/bin/nohup /home/linbpq/linbpq 2>&1 >> /dev/null)&
+
+# Linbpq opens ports 8008 and 8010 and 8011
+
+# NOTE: MUST exit with success so /etc/rc.local will not get mad!
+exit 0
+
